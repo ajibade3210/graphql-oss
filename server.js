2c3b24ab8fe1357d080d4bca6447e4d4fcc63043
laolu-ajibade <ajibade.olunowo@scelloo.com>
2022-12-14 13:33:45 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const data = {
+  Todo: [
+    { id: "001", name: "Make money" },
+    { id: "002", name: "Rent apartment" },
+    { id: "003", name: "Make more more" },
+    { id: "004", name: "Get girlfriend" },
+  ],
+};
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.send("Hello, GraphQL!");
+});
+
+app.listen(port, () => {
+  console.log(`Running a server at http://localhost:${port}`);
+});
